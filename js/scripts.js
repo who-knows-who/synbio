856840577d1c6b37815b0c7f8c1a612dd43f8b84
who-knows-who <j.williams.8@warwick.ac.uk>
2018-01-04 03:14:47 +0000

    jQuery UI reordering added

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,16 @@
 var headers = ['Type', 'Name', 'Direction', 'Description', 'Remove'];
 
+
 function createTable() {
 
     var table = document.createElement('table');
     table.setAttribute('id', 'partsTable');
 
-    var tr = table.insertRow(-1);
+    var tableHead = document.createElement('thead');
+
+    var tr = document.createElement('tr');
+    tableHead.appendChild(tr);
+    table.appendChild(tableHead);
 
     for (var i = 0; i < headers.length; i++) {
         var th = document.createElement('th');
@@ -13,17 +18,26 @@ function createTable() {
         tr.appendChild(th);
     }
 
+    var tableBody = document.createElement('tbody');
+    tableBody.setAttribute('id', 'tableBody');
+    table.appendChild(tableBody);
+
+
     var div = document.getElementById('parts');
     div.appendChild(table);
+
+    $("#sortable").sortable();
+    $("#tableBody").sortable();
 
 }
 
 function addPart() {
 
-    var table = document.getElementById('partsTable');
-    var rowCnt = table.rows.length;
+    var tableBody = document.getElementById('tableBody');
+    var rowCnt = document.getElementById('partsTable').rows.length;
 
-    var tr = table.insertRow(rowCnt);
+    var tr = document.createElement('tr');
+    tableBody.appendChild(tr);
 
     for (var i = 0; i < headers.length; i++) {
 
@@ -37,7 +51,7 @@ function addPart() {
             case 'Name':
                 var text = document.createElement('input');
                 text.setAttribute('type', 'text');
-                text.setAttribute('value', 'rowCnt');
+                text.setAttribute('value', rowCnt.toString());
                 td.appendChild(text);
                 break;
             case 'Direction':
